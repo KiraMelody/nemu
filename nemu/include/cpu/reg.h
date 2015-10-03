36f0c86810b0f2f6bb1101b08b28ff4d63a49b41
tracer-ics2015 <tracer@njuics.org>
2015-10-03 21:18:39 +0800

    >  compile NEMU 141220000 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:18:39 up  2:00,  2 users,  load average: 0.08, 0.03, 0.05 3e567126c40ac2f97a276b6c97cbee83e112b33

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,18 +15,17 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-	struct {
+	union {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-
+    struct {
 	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
 	swaddr_t eip;
-
+	};
 } CPU_state;
 
 extern CPU_state cpu;
