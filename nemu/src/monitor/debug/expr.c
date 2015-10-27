f356637e48ceb47884dc88a8efafe33e00cbb17a
tracer-ics2015 <tracer@njuics.org>
2015-10-28 00:18:05 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:18:05 up 15:39,  2 users,  load average: 0.12, 0.10, 0.12 e6525afa67c21713f6ca3aeaaf6ea7ba96a9babf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -245,6 +245,7 @@ uint32_t expr(char *e, bool *success) {
   	}
 	/* TODO: Insert codes to evaluate the expression. */	
 	*success = true;
+	printf ("get eval!\n");
 	return eval (0,nr_token-1);
 }
 
