809b4707865ab865244528eadc2beaada3538b54
tracer-ics2015 <tracer@njuics.org>
2015-10-28 09:57:01 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  09:57:01 up 17:14,  2 users,  load average: 0.08, 0.05, 0.07 74f2318398e32909b33a6c822c50942c2e9c8ca

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -86,6 +86,7 @@ static bool make_token(char *e) {
 	while(e[position] != '\0') {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
+			printf ("try %d\n",i);
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
