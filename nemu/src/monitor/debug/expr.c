31a8d4ea5668125fc0acbf5f0e167aae14c3696b
tracer-ics2015 <tracer@njuics.org>
2015-11-03 00:48:18 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:48:18 up  6:31,  3 users,  load average: 0.05, 0.06, 0.05 7e9ef8ee65130a869e47b5044dbe2a6aa745c1da

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -108,6 +108,7 @@ static bool make_token(char *e) {
 						token[nr_token].type = rules[i].token_type;
 						token[nr_token].priority = rules[i].priority;
 						strncpy (token[nr_token].str,substr_start,substr_len);
+						token[nr_token].str[substr_len]='\0';
 						printf ("%s\n",token[nr_token].str);
 						nr_token ++;
 				}
--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -105,7 +105,7 @@ static int cmd_b(char *args) {
 	swaddr_t addr;
 	addr = expr (args+1,&suc);
 	if (!suc)assert (1);
-	sprintf (args,"$eip == *0x%x",addr);
+	sprintf (args,"$eip == 0x%x",addr);
 	printf ("Breakpoint %d at 0x%x\n",breakpoint_counter,addr);
 	breakpoint_counter++;
 	WP *f;
