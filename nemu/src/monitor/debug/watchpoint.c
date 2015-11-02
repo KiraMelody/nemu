e6ef0ef7278857eb6bbb20228e886ed7046e8576
tracer-ics2015 <tracer@njuics.org>
2015-11-03 00:53:26 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:53:26 up  6:37,  3 users,  load average: 0.21, 0.11, 0.07 97c392ec416675b28292dc176643f5c215fec03d

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -26,9 +26,6 @@ WP* new_wp()
 	free_ = free_->next;
 	f->next = head;
 	head = f;
-	if (f->next == NULL)printf ("YES\n");
-	else
-	printf ("%d -> %d\n",f->NO,f->next->NO);
 	return f;
 }
 void free_wp (WP *wp)
