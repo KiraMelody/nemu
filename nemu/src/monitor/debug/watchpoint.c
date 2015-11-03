1733b5582ff2bb4395b2c08fe36ead7e3457c179
tracer-ics2015 <tracer@njuics.org>
2015-11-03 18:03:33 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:03:33 up  8:52,  3 users,  load average: 0.06, 0.04, 0.05 f7c1df36eeb1fa4fecc52205a4ed8e5fb13a10

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -21,32 +21,45 @@ void init_wp_list() {
 /* TODO: Implement the functionality of watchpoint */
 WP* new_wp()
 {
-	WP *f;
+	WP *f,*p;
 	f = free_;
 	free_ = free_->next;
-	f->next = head;
-	head = f;
-	if (head->next == NULL)printf (" %d yeyeye\n",head->NO);
+	f->next = NULL;
+	p = head;
+	if (p == NULL){head = f;p = head;}
+	else {
+		while (p->next!=NULL)p=p->next;
+		p->next = f;
+		}
 	return f;
 }
 void free_wp (WP *wp)
 {
-	WP *f;
-	f = head;
-	while (f->next != NULL && f->next->NO != wp->NO)f = f->next;
-	if (f->next == NULL)
-	{
-		printf ("%d\n",head->NO);
-		if (head->NO == wp->NO)head = head->next;
-		if (head == NULL)printf ("NULL\n");
+	WP *f,*p;
+	p = free_;
+	if (p == NULL){free_ = wp;p = free_;}
+	else {
+		while (p->next!=NULL)p=p->next;
+		p->next = wp;
 	}
+	f = head;
+	if (head == NULL)assert (0);
+	if (head->next == NULL && head->NO == wp->NO)
+	{
+		head = NULL;
+	}
+	else 
+	{
+		while (f->next != NULL && f->next->NO != wp->NO)f = f->next;
+	if (f->next == NULL)
+		printf ("what ghost\n");
 	else if (f->next->NO == wp->NO)f->next = f->next->next;
 	else assert (0);
-	if (head == NULL)printf ("OK\n");
-	wp->next = free_;
+	if (head == NULL)printf ("*NULL\n");
+	}
+	wp->next = NULL;
 	wp->val = 0;
 	wp->expr[0] = '\0';
-	free_ = wp;
 }
 bool check_wp()
 {
