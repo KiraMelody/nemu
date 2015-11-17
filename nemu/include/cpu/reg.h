690a4b44deb65d380162e38f378d0915a3afe5b5
tracer-ics2015 <tracer@njuics.org>
2015-11-18 00:27:42 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:27:42 up 2 days, 20:38,  2 users,  load average: 0.22, 0.17, 0.14 2ed752c3fd8d796f7bc9198f696a277a3acd13b5

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -22,9 +22,29 @@ typedef union {
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-    struct {
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-	swaddr_t eip;
+    	struct {
+		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		swaddr_t eip;
+		struct{
+			uint32_t :14;
+			uint32_t VM:1;
+			uint32_t RF:1;
+			uint32_t :1;
+			uint32_t NT:1;
+			uint32_t IOPL:2;
+			uint32_t OF:1;
+			uint32_t DF:1;
+			uint32_t IF:1;
+			uint32_t TF:1;
+			uint32_t SF:1;
+			uint32_t ZF:1;
+			uint32_t :1;
+			uint32_t AF:1;
+			uint32_t :1;
+			uint32_t PF:1;
+			uint32_t :1;
+			uint32_t CF:1;
+		};
 	};
 } CPU_state;
 
