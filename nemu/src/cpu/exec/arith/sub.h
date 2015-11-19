1ac237dd5b8262275a5b9ceaef5775df440167d2
tracer-ics2015 <tracer@njuics.org>
2015-11-19 19:05:57 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:05:57 up  5:33,  2 users,  load average: 0.16, 0.18, 0.15 6c4b8eae3fb160082f825d1c6dc83f26b08d635

--- nemu/src/cpu/exec/arith/sub.h
+++ nemu/src/cpu/exec/arith/sub.h
@@ -1,19 +1,19 @@
 #ifndef __SUB_H__
 #define __SUB_H__
-/*
-make_helper(sub_i2a_b)
-make_helper(sub_i2rm_b)
-make_helper(sub_r2rm_b)
-make_helper(sub_rm2r_b)
+
+make_helper(sub_i2a_b);
+make_helper(sub_i2rm_b);
+make_helper(sub_r2rm_b);
+make_helper(sub_rm2r_b);
 
 
-make_helper(sub_i2a_v)
-make_helper(sub_i2rm_v)
-make_helper(sub_r2rm_v)
-make_helper(sub_rm2r_v)
-*/
+make_helper(sub_i2a_v);
+make_helper(sub_i2rm_v);
+make_helper(sub_r2rm_v);
+make_helper(sub_rm2r_v);
+
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-make_helper(sub_si2rm_v)
+make_helper(sub_si2rm_v);
 #endif
 
 #endif
