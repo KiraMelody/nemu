9040668a26a9ecc4fbd1c5b421ec4e6d3660915d
tracer-ics2015 <tracer@njuics.org>
2015-11-19 18:39:59 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:39:59 up  5:07,  2 users,  load average: 0.05, 0.10, 0.13 6c0846b787409abc2e275fcd5f9b002db1ffef45

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -23,9 +23,9 @@ make_instr_helper(si2rm)
 #endif
 
 make_instr_helper(i2a)
-make_instr_helper(i2r)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
+make_instr_helper(rm2r)
 
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/sub.c
+++ nemu/src/cpu/exec/arith/sub.c
@@ -15,5 +15,5 @@
 /* for instruction encoding overloading */
 make_helper_v(sub_i2a)
 make_helper_v(sub_i2rm)
-//make_helper_v(sub_rm2r)
 make_helper_v(sub_r2rm)
+make_helper_v(sub_rm2r)
