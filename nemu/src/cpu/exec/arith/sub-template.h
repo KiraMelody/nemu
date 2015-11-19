3c343b58b7d30e9fbdb3c03b6524adba2cb4aaf7
tracer-ics2015 <tracer@njuics.org>
2015-11-19 18:16:56 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:16:56 up  4:44,  2 users,  load average: 0.10, 0.10, 0.14 28215a4976ecd03a2254556d715e7b868db6fc2

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -1,7 +1,7 @@
 #include "cpu/exec/template-start.h"
 
 #define instr sub
-/*
+
 static void do_execute() {
 	uint32_t result = op_dest->val - op_src->val;
 	OPERAND_W(op_dest, result);
@@ -26,6 +26,6 @@ make_instr_helper(i2a)
 make_instr_helper(i2r)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
-*/
+
 
 #include "cpu/exec/template-end.h"
