de4c208c4dd6f7ab73f710eecb202831db6d67f7
tracer-ics2015 <tracer@njuics.org>
2015-11-19 18:56:37 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:56:37 up  5:24,  2 users,  load average: 0.07, 0.09, 0.13 42be9b46d8ea3f76e7a8afc6e548a88eb0f12198

--- nemu/src/cpu/exec/arith/sub.h
+++ nemu/src/cpu/exec/arith/sub.h
@@ -3,12 +3,14 @@
 /*
 make_helper(sub_i2a_b)
 make_helper(sub_i2rm_b)
-make_helper(sub_rm2r_b)
 make_helper(sub_r2rm_b)
+make_helper(sub_rm2r_b)
+
 
 make_helper(sub_i2a_v)
 make_helper(sub_i2rm_v)
-make_helper(sub_rm2r_v)
 make_helper(sub_r2rm_v)
+make_helper(sub_rm2r_v)
 */
+
 #endif
