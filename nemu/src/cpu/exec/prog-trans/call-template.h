09d2a343fe07327d8ccfa6e47f6b6ebfe9a62da6
tracer-ics2015 <tracer@njuics.org>
2015-11-20 10:13:54 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:13:54 up 10:09,  2 users,  load average: 0.09, 0.15, 0.15 e3653b8945df575b7ef6772e8ce5720367c93b06

--- nemu/src/cpu/exec/prog-trans/call-template.h
+++ nemu/src/cpu/exec/prog-trans/call-template.h
@@ -6,7 +6,7 @@ make_helper(exec);
 
 static void do_execute() {
 	uint32_t displacement = instr_fetch(cpu.eip, DATA_BYTE);
-	printf ("0x%x",cpu.eip);
+	printf ("0x%x + 0x%x\n",cpu.eip,displacement);
 	print_asm_template1();
 	exec(cpu.eip + displacement);
 }
