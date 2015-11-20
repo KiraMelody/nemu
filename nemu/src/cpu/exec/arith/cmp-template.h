0f22ab6832cfd87d0c8c5aecb94b600d48fc25c2
tracer-ics2015 <tracer@njuics.org>
2015-11-20 15:22:26 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:22:26 up 13:53,  2 users,  load average: 0.72, 0.26, 0.12 6defb2358ad2bef12cd7a7e03d4f60daab7b58a6

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -7,16 +7,13 @@ static void do_execute() {
 	uint32_t result = op_dest->val - op_src->val;
 	int len = (DATA_BYTE << 3) - 1;
 	cpu.CF = op_dest->val < op_src->val;
-	int x = result >> len;
-	cpu.SF=x;
-    	x = x + (x & 1);
-    	cpu.OF=!x;
+	cpu.SF=result >> len;
+    	cpu.OF=(op_dest->val >= op_src->val && cpu.SF) || (op_dest->val < op_src->val && !cpu.SF);
 	cpu.ZF=!result;
 	result ^= result >>4;
 	result ^= result >>2;
 	result ^= result >>1;
 	cpu.PF=!(result & 1);
-	printf ("%d\n",cpu.ZF);
 	print_asm_no_template2();
 }
 #if DATA_BYTE == 2 || DATA_BYTE == 4
--- nemu/src/cpu/exec/prog-trans/je-template.h
+++ nemu/src/cpu/exec/prog-trans/je-template.h
@@ -5,7 +5,7 @@
 
 static void do_execute() {
 	uint32_t displacement = instr_fetch(cpu.eip + 1, DATA_BYTE);
-	print_asm("je %x\n",cpu.eip + 1 + DATA_BYTE + displacement);
+	print_asm("je %x",cpu.eip + 1 + DATA_BYTE + displacement);
 	if (cpu.ZF == 1)cpu.eip +=displacement;
 }
 make_instr_helper(i)
