cd10e3b31f563f30bd34654f0091a90c8527d298
tracer-ics2015 <tracer@njuics.org>
2015-11-24 15:49:33 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:49:33 up 13:59,  3 users,  load average: 0.26, 0.16, 0.15 13c5971047feeb2ec2b647b22e3dc3cdc23d8240

--- nemu/src/cpu/exec/prog-trans/jbe-template.h
+++ nemu/src/cpu/exec/prog-trans/jbe-template.h
@@ -5,7 +5,7 @@
 
 static void do_execute() {
 	int32_t displacement = instr_fetch(cpu.eip + 1, DATA_BYTE);
-	displacement &= 0xffff;
+	//displacement &= 0xff;
 	print_asm("jbe %x",cpu.eip + 1 + DATA_BYTE + displacement);
 	if (cpu.CF == 1 || cpu.ZF == 1)cpu.eip +=displacement;
 }
