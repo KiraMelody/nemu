86a3ca0450c5701f4acbb7d15fdf736bdb16e3e6
tracer-ics2015 <tracer@njuics.org>
2015-11-25 00:18:52 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:18:52 up 19:29,  3 users,  load average: 0.09, 0.21, 0.19 4cd6e16b839db304e4ab627b885141495e0839b3

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -31,7 +31,6 @@ void init_monitor(int argc, char *argv[]) {
 
 	/* Load the string table and symbol table from the ELF file for future use. */
 	load_elf_tables(argc, argv);
-
 	/* Compile the regular expressions. */
 	init_regex();
 
