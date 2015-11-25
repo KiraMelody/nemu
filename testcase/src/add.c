ee8813078c6c10fa3429318c4dc0ab010407f300
tracer-ics2015 <tracer@njuics.org>
2015-11-25 16:52:17 +0800

    >  run 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:52:17 up 1 day, 50 min,  2 users,  load average: 0.01, 0.04, 0.05 14b5648c915fbd6b50d37cc1a6f5cbf8957a93b

--- testcase/src/add.c
+++ testcase/src/add.c
@@ -12,8 +12,8 @@ int ans[] = {0, 0x1, 0x2, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffff
 
 int main() {
 	int i, j, ans_idx = 0;
-	for(i = 1; i < NR_DATA; i ++) {
-		for(j = 1; j < NR_DATA; j ++) {
+	for(i = 0; i < NR_DATA; i ++) {
+		for(j = 0; j < NR_DATA; j ++) {
 			nemu_assert(add(test_data[i], test_data[j]) == ans[ans_idx ++]);
 		}
 	}
