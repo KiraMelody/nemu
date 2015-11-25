1179b92a2858ce868ca9bd15579af8c3c02e0ead
tracer-ics2015 <tracer@njuics.org>
2015-11-25 15:18:38 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:18:38 up 23:24,  2 users,  load average: 0.37, 0.20, 0.15 5fb75b79b92c69d79ae4f425a65b9fd7fdf004bb

--- nemu/src/cpu/exec/data-mov/leave-template.h
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -4,8 +4,8 @@
 
 static void do_execute () {
 	printf ("leave %d to esp\n",REG (5) );
-	REG(4) = REG (5);
-	REG (5) = MEM_R (REG(5));
+	REG(R_ESP) = REG (R_EBP);
+	REG(R_EBP)+=DATA_BYTE;
 	printf ("now ebp = %d\n",REG(5));
 	print_asm("leave");
 }
