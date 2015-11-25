b899af2b86f9c71e45e10203d10bbd2fe7b24d25
tracer-ics2015 <tracer@njuics.org>
2015-11-25 15:30:10 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:30:10 up 23:35,  2 users,  load average: 0.24, 0.13, 0.14 bd85943b977bb464d763f9cfd37da5b4cccc811

--- nemu/src/cpu/exec/prog-trans/call-template.h
+++ nemu/src/cpu/exec/prog-trans/call-template.h
@@ -4,6 +4,7 @@
 
 
 static void do_execute() {
+	printf ("esp %d to %d\n",REG (R_ESP),REG (R_ESP)-DATA_BYTE);
 	REG (R_ESP) -= DATA_BYTE;
 	MEM_W (REG (R_ESP) , cpu.eip + DATA_BYTE);
 	DATA_TYPE_S displacement = op_src->val;
