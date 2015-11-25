578c7d7b17d704eeb556dc160c17267e3cd1c47a
tracer-ics2015 <tracer@njuics.org>
2015-11-25 23:25:54 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:25:54 up 1 day,  6:04,  3 users,  load average: 0.00, 0.10, 0.16 c921f464eeadd5a2c7ff8382fd1a1c3d5952a592

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -64,7 +64,7 @@ void load_elf_tables(int argc, char *argv[]) {
 			assert(ret == 1);
 			nr_symtab_entry = sh[i].sh_size / sizeof(symtab[0]);
 			for (j=0;j<nr_symtab_entry;j++)
-			printf("stname = %d\n", symtab[j].st_name);
+			printf("st_info = %d\n", symtab[j].st_info);
 		}
 		else if(sh[i].sh_type == SHT_STRTAB && 
 				strcmp(shstrtab + sh[i].sh_name, ".strtab") == 0) {
