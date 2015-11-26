a5cc75c8f1568397a7410a2f273db1725c27784e
tracer-ics2015 <tracer@njuics.org>
2015-11-26 12:43:17 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:43:17 up 1 day,  9:52,  3 users,  load average: 0.12, 0.18, 0.13 245ba22166b7d910b48e47de4d900e31afbac469

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -163,7 +163,7 @@ static int cmd_bt(char *args) {
 				break;
 			}
 		}
-		printf("%s ",tmp);
+		printf("%s\t",tmp);
 		if (strcmp (tmp,"main") == 0)printf ("( )\n");
 		else printf ("( %d , %d , %d , %d )\n", now_ebp.args[0],now_ebp.args[1],now_ebp.args[2],now_ebp.args[3]);
 		read_ebp (addr,&now_ebp);
