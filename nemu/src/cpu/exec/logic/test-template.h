134e11ab3e19f12b932381ef283257a53f67ff5d
tracer-ics2015 <tracer@njuics.org>
2015-11-26 13:56:37 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  13:56:37 up 1 day, 10:26,  3 users,  load average: 0.23, 0.24, 0.19 b608c864c8cb442c716307d13d77fe73f34e6658

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -3,7 +3,7 @@
 #define instr test
 
 static void do_execute() {
-	uint32_t result = op_dest->val & op_src->val;
+	DATA_TYPE result = op_dest->val & op_src->val;
 	cpu.CF=0;
 	cpu.OF=0;
 	cpu.ZF=!result;
