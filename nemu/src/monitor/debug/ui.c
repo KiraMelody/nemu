70b09c1564c6b5669c9390905c73a899777a3bf1
tracer-ics2015 <tracer@njuics.org>
2015-11-26 12:28:19 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:28:19 up 1 day,  9:37,  3 users,  load average: 0.35, 0.14, 0.07 c2ef6fe2200021281310b12f9f4261044733ec8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -153,9 +153,9 @@ static int cmd_bt(char *args) {
 		printf ("#%d  0x%08x in ",j++,now_ebp.ret_addr);
 		for (i=0;i<nr_symtab_entry;i++)
 		{
+			printf ("0x%08x : 0x%08x\n",symtab[i].st_value,symtab[i].st_value +  symtab[i].st_size);
 			if (symtab[i].st_value <= now_ebp.ret_addr && symtab[i].st_value +  symtab[i].st_size >= now_ebp.ret_addr && (symtab[i].st_info&0xf) == STT_FUNC)
 			{
-				//printf ("0x%08x : 0x%08x\n",symtab[i].st_value,symtab[i].st_value +  symtab[i].st_size);
 				tmplen = symtab[i+1].st_name - symtab[i].st_name - 1;
 				strncpy (tmp,strtab+symtab[i].st_name,tmplen);
 				tmp [tmplen] = '\0';
