f79a4805dffe4e36c41945383ba01748094c33a6
tracer-ics2015 <tracer@njuics.org>
2015-11-27 18:35:11 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:35:11 up 2 days,  1:21,  3 users,  load average: 0.15, 0.17, 0.15 80b03ff92043ea653d4b79d13ddad4976a75

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -4,6 +4,7 @@
 
 static void do_execute() {
 	DATA_TYPE_S result = op_src->val;
+	printf ("src.size = %d,dest.size = %d\n",(int)op_src->size,(int)op_dest->size);
 	if (op_src->size == 1)result &= 0xff;
 	printf ("%d : %x -> %x\n",DATA_BYTE,op_src->val,result);
 	OPERAND_W(op_dest, result);
