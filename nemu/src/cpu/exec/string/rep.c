4056bc6bb3c94ec0023cfc2814ce50e542ffb566
tracer-ics2015 <tracer@njuics.org>
2015-11-27 20:29:00 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:29:00 up 2 days,  3:15,  3 users,  load average: 0.18, 0.15, 0.14 817944ed99a9019abc82512d2e86aea49e71dca1

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -17,7 +17,7 @@ make_helper(rep) {
 			exec(eip + 2);
 			count ++;
 			cpu.ecx --;
-			assert(ops_decoded.opcode == 0xa4	// movsb
+			/*assert(ops_decoded.opcode == 0xa4	// movsb
 				|| ops_decoded.opcode == 0xa5	// movsw
 				|| ops_decoded.opcode == 0xaa	// stosb
 				|| ops_decoded.opcode == 0xab	// stosw
@@ -25,7 +25,7 @@ make_helper(rep) {
 				|| ops_decoded.opcode == 0xa7	// cmpsw
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
-				);
+				);*/
 			if ((ops_decoded.opcode == 0xa4	
 				|| ops_decoded.opcode == 0xa5	
 				|| ops_decoded.opcode == 0xaa	
