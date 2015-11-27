69cebc00461e4be3ad3e31efc5f6df5eb0514211
tracer-ics2015 <tracer@njuics.org>
2015-11-28 00:56:15 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:56:15 up  2:59,  2 users,  load average: 0.13, 0.14, 0.14 b868b783ff7afbf78dd8a41cf249875d8e4e53bd

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,13 +5,11 @@
 static void do_execute() {
 	DATA_TYPE_S result;
 	printf ("size %d %d\n",(int)op_src->size,(int)op_src->size);
-	if (op_dest->size == 1)result = (int8_t)(op_src->val & 0xff);
-	else if (op_dest->size == 2)result = (int16_t)(op_src->val &0xffff);
-	else result = (int32_t)op_src->val;
+	if (ops_decoded.opcode == 0xbe)result = (int8_t)(op_src->val & 0xff);
+	else result = (int16_t)(op_src->val &0xffff);
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
-
 
 make_instr_helper(rm2r)
 
