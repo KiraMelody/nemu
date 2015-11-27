6aa47cd658a4cd46879ff84906ee9c6decdb1f82
tracer-ics2015 <tracer@njuics.org>
2015-11-27 12:49:02 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:49:02 up 1 day, 20:16,  3 users,  load average: 0.07, 0.07, 0.05 6e3466ba81052d5251fc2150ce26814399ae7127

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -3,6 +3,7 @@
 #define instr push
 
 static void do_execute () {
+	printf("push byte %d\n", DATA_BYTE);
 	if (DATA_BYTE == 1)
 	{
 		REG (R_ESP) -= 4;
@@ -10,8 +11,8 @@ static void do_execute () {
 	}
 	else
 	{
-	REG (R_ESP) -= DATA_BYTE;
-	MEM_W (REG (R_ESP) , op_src->val);
+		REG (R_ESP) -= DATA_BYTE;
+		MEM_W (REG (R_ESP) , op_src->val);
 	}
 	print_asm_no_template1();
 }
