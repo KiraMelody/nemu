920043b20382a38b32e51814bb561822b2aaa902
tracer-ics2015 <tracer@njuics.org>
2015-11-27 13:54:24 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  13:54:24 up 1 day, 21:21,  3 users,  load average: 0.13, 0.14, 0.14 ad7f814122862a04d5c2d5e4eed1e3685331ce17

--- nemu/src/cpu/exec/prog-trans/jle-template.h
+++ nemu/src/cpu/exec/prog-trans/jle-template.h
@@ -6,7 +6,7 @@
 static void do_execute() {
 	DATA_TYPE_S displacement = op_src->val;
 	print_asm("jle %x",cpu.eip + 1 + DATA_BYTE + displacement);
-	if (cpu.ZF == 1 && cpu.SF != cpu.OF)cpu.eip +=displacement;
+	if (cpu.ZF == 1 || cpu.SF != cpu.OF)cpu.eip +=displacement;
 }
 make_instr_helper(i)
 
