e40385b407a53f07056c991a6b94bbafe3a94898
tracer-ics2015 <tracer@njuics.org>
2015-11-27 18:55:42 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:55:42 up 2 days,  1:42,  3 users,  load average: 0.48, 0.21, 0.16 fb714e129d9a9f5baae927df98e40e6171a79c5

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	DATA_TYPE_S result;
 	printf ("%lu",sizeof (DATA_TYPE_S));
 	printf ("src.size = %d,dest.size = %d\n",(int)op_src->size,(int)op_dest->size);
-	if (op_dest->size == 1)result = (DATA_TYPE_S)(op_dest->val &0xff);
+	if (op_dest->size == 1)result = (int32_t)(op_dest->val & 0xff);
 	else if (op_dest->size == 2)result = (DATA_TYPE_S)(op_dest->val &0xffff);
 	else result = (DATA_TYPE_S)op_dest->val;
 	printf ("%d : %x -> %x\n",DATA_BYTE,op_dest->val,result);
