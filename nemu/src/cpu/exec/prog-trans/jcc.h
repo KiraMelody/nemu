89342c1753ea567a61c84afbd2d52f5caa442bdf
tracer-ics2015 <tracer@njuics.org>
2015-11-27 15:17:11 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:17:11 up 1 day, 22:04,  3 users,  load average: 0.31, 0.17, 0.15 36579f9a67f1ecf2ae636429295eace93ec696b0

--- nemu/src/cpu/exec/prog-trans/jcc.h
+++ nemu/src/cpu/exec/prog-trans/jcc.h
@@ -1,6 +1,7 @@
 #include "jbe.h"
 #include "je.h"
 #include "jg.h"
+#include "jl.h"
 #include "jne.h"
 #include "jle.h"
 #include "jmp.h"
---
+++ nemu/src/cpu/exec/prog-trans/jl.h
@@ -0,0 +1,7 @@
+#ifndef __JL_H__
+#define __JL_H__
+
+make_helper(jl_i_b);
+make_helper(jl_i_v);
+
+#endif
