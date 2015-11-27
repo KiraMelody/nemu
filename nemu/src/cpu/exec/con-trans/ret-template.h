be8a271ce28a6649767809b78729392e7cea4c27
tracer-ics2015 <tracer@njuics.org>
2015-11-27 23:59:01 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:59:01 up  2:02,  2 users,  load average: 0.13, 0.13, 0.14 3266d0b0a670e88d15ca4875f7792e39fabbe6ed

--- nemu/src/cpu/exec/con-trans/ret-template.h
+++ nemu/src/cpu/exec/con-trans/ret-template.h
@@ -6,18 +6,19 @@ static void do_execute () {
 	cpu.eip = MEM_R (REG (R_ESP));
 	if (DATA_BYTE == 2)cpu.eip &= 0xffff;
 	REG (R_ESP) += DATA_BYTE;
-	if (op_src->type == OP_TYPE_IMM)
-	{
-		swaddr_t i;
-		op_src->val &= op_src->imm &0xffff;
-		for (i = 0;i < op_src->val; i+=DATA_BYTE)
-		MEM_W (REG (R_ESP) + i,0);
-		REG (R_ESP) += op_src->val;
-		print_asm_no_template1();
-	}		
-	else print_asm("ret");
+	print_asm("ret");
 }
 make_instr_helper(n)
-make_instr_helper(i)
+
+make_helper(concat(ret_i_, SUFFIX))
+{
+	int val = instr_fetch(eip + 1, 2);
+	int i;
+	for (i = 0;i < val; i+=DATA_BYTE)
+	MEM_W (REG (R_ESP) + i,0);
+	REG (R_ESP) += val;
+	print_asm_no_template1();
+	return 3;
+}
 
 #include "cpu/exec/template-end.h"
