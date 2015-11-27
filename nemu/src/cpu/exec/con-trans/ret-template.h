16bef28d0f42aa268693a392c51e658edcc11d5e
tracer-ics2015 <tracer@njuics.org>
2015-11-27 22:28:01 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:28:01 up 31 min,  2 users,  load average: 0.16, 0.11, 0.13 4c9bc09a311cf4ef492a01ed77daeadfdffc566

--- nemu/src/cpu/exec/con-trans/ret-template.h
+++ nemu/src/cpu/exec/con-trans/ret-template.h
@@ -4,11 +4,13 @@
 
 static void do_execute () {
 	cpu.eip = MEM_R (REG (R_ESP));
+	printf ("EIP = %d\n",cpu.eip);
 	if (DATA_BYTE == 2)cpu.eip &= 0xffff;
 	REG (R_ESP) += DATA_BYTE;
 	if (op_src->type == OP_TYPE_IMM)
 	{
 		swaddr_t i;
+		printf ("%d\n",op_src->val);
 		for (i = 0;i < op_src->val; i+=DATA_BYTE)
 		MEM_W (REG (R_ESP) + i,0);
 		REG (R_ESP) += op_src->val;
