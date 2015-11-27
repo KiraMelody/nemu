1f492851714c172e59034d4b6b4a9b219873c68b
tracer-ics2015 <tracer@njuics.org>
2015-11-27 19:52:16 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:52:16 up 2 days,  2:39,  3 users,  load average: 0.05, 0.09, 0.16 dbf384e059236962b8da4488519432e79f9604f

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -96,9 +96,9 @@ make_helper(concat(decode_r2rm_, SUFFIX)) {
  //change from return decode_rm_internal(eip, op_src, op_dest);
 make_helper(concat(decode_rm2r_, SUFFIX)) {
 	int len = decode_rm_internal(eip, op_src, op_dest);
-	size_t si = op_src->size;
-	op_src->size = op_dest->size;
-	op_dest->size = si;
+	//size_t si = op_src->size;
+	//op_src->size = op_dest->size;
+	//op_dest->size = si;
 	return len;
 }
 
