1aa62fb6dfea98b5540e273affa50ab4929a307d
tracer-ics2015 <tracer@njuics.org>
2015-11-27 19:44:16 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:44:16 up 2 days,  2:31,  3 users,  load average: 0.15, 0.18, 0.21 8d82f12e3c3376e646cc17288d166faaed1f95f6

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	//printf ("%d : %d\n",op_src->val,op_dest->val);
 	DATA_TYPE result = op_src->val;
 	OPERAND_W(op_dest, result);
-	//printf ("%d : %d\n",(int)op_src->size,(int)op_dest->size);
+	printf ("%d : %d\n",(int)op_src->size,(int)op_dest->size);
 	print_asm_template2();
 }
 
