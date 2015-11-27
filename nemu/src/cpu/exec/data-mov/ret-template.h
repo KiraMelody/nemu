fc94efa4acb9494874d7baf4a5ffa57f7f67210d
tracer-ics2015 <tracer@njuics.org>
2015-11-27 11:10:17 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:10:17 up 1 day, 19:07,  3 users,  load average: 0.36, 0.24, 0.16 9a4422b8996dd7cd679ac36cfcc190bd66d1163d

--- nemu/src/cpu/exec/data-mov/ret-template.h
+++ nemu/src/cpu/exec/data-mov/ret-template.h
@@ -4,7 +4,10 @@
 
 static void do_execute () {
 	cpu.eip = MEM_R (REG (R_ESP));
+	if (DATA_BYTE == 2)cpu.eip &= 0xffff;
 	REG (R_ESP) += DATA_BYTE;
+	if (op_src->type == OP_TYPE_IMM)
+		REG (R_ESP) += op_src->val;
 	print_asm("ret");
 }
 make_instr_helper(n)
