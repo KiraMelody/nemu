d0eeceb542e1221a53f99b91e381e828bd91390b
tracer-ics2015 <tracer@njuics.org>
2015-11-27 20:05:05 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:05:05 up 2 days,  2:51,  3 users,  load average: 0.22, 0.17, 0.15 fe46168cec349a178d2e5942dea459552bc2d960

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -196,7 +196,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x80 */	inv, inv, inv, inv,
 /* 0x84 */	inv, inv, inv, inv,
 /* 0x88 */	inv, inv, inv, inv, 
-/* 0x8c */	inv, inv, jle_i_v, inv, 
+/* 0x8c */	jl_i_v, inv, jle_i_v, inv, 
 /* 0x90 */	inv, inv, inv, inv,
 /* 0x94 */	inv, setne_rm_b, inv, inv,
 /* 0x98 */	inv, inv, inv, inv, 
