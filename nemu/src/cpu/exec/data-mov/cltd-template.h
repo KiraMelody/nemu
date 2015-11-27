7735e41edf5d2bea693266c6ba250947d4aa1c5f
tracer-ics2015 <tracer@njuics.org>
2015-11-27 16:03:18 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:03:18 up 1 day, 22:50,  3 users,  load average: 0.14, 0.12, 0.14 b9e3a1938db2c219da734bf0b9b27e53443ee733

--- nemu/src/cpu/exec/data-mov/cltd-template.h
+++ nemu/src/cpu/exec/data-mov/cltd-template.h
@@ -8,11 +8,11 @@ static void do_execute () {
 	printf ("0x%x",result);
 	if (REG(R_EAX) < 0)
 	{
-		MEM_W (REG (R_EDX) , result);
+		REG (R_EDX) = result;
 	}
 	else
 	{
-		MEM_W (REG (R_EDX) , 0);
+		REG (R_EDX) = 0;
 	}
 	print_asm("cltd");
 }
