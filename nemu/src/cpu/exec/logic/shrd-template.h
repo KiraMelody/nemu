5fce05510ea170f77a27ae895a110a252208b336
tracer-ics2015 <tracer@njuics.org>
2015-11-28 23:42:35 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:42:35 up 14:52,  2 users,  load average: 0.31, 0.20, 0.14 372b18d458bcba5592610faa7a973e0decbde901

--- nemu/src/cpu/exec/logic/shrd-template.h
+++ nemu/src/cpu/exec/logic/shrd-template.h
@@ -2,12 +2,11 @@
 
 #define instr shrd
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
-static void do_execute () {
-	printf ("%d %d %d\n",op_src->reg,op_dest->reg,op_src2->reg);
-;	DATA_TYPE in = op_dest->val;
+#if DATA_BYTE == 2 
+static void do_execute_w () {
+	DATA_TYPE in = op_dest->val;
 	DATA_TYPE out = op_src2->val;
-	uint8_t count = MEM_R (reg_b (R_CL));
+	uint8_t count = op_src->val;
 	count &= 0x1f;
 	while(count != 0) {
 		out >>= 1;
@@ -24,18 +23,77 @@ static void do_execute () {
 make_helper(concat(shrd_i_, SUFFIX)) {
 	int len = concat(decode_si_rm2r_, SUFFIX) (eip + 1);  /* use decode_si_rm2r to read 1 byte immediate */
 	op_dest->val = REG(op_dest->reg);
-	do_execute();
+	do_execute_w();
 	return len + 1;
 }
 
+static void do_execute_cl_w () {
+	DATA_TYPE in = op_src->val;
+	DATA_TYPE out = op_dest->val;
+	uint8_t count = MEM_R (reg_b (R_CL));
+	count &= 0x1f;
+	while(count != 0) {
+		out >>= 1;
+		out |= (in & 1) << ((DATA_BYTE << 3) - 1);
+		in >>= 1;
+		count --;
+	}
+
+	OPERAND_W(op_dest, out);
+
+	print_asm("shrd" str(SUFFIX) " $cl,%s,%s", op_dest->str, op_src->str);
+}
 make_helper(concat(shrd_cl_, SUFFIX)) {
 	int len = concat(decode_rm2r_, SUFFIX) (eip + 1);
+	do_execute_cl_w();
+	return len + 1;
+}
+#endif
+
+#if DATA_BYTE == 4
+static void do_execute_l () {
+	DATA_TYPE in = op_dest->val;
+	DATA_TYPE out = op_src2->val;
+	uint8_t count = op_src->val;
+	count &= 0x1f;
+	while(count != 0) {
+		out >>= 1;
+		out |= (in & 1) << ((DATA_BYTE << 3) - 1);
+		in >>= 1;
+		count --;
+	}
+
+	OPERAND_W(op_src2, out);
+
+	print_asm("shrd" str(SUFFIX) " %s,%s,%s", op_src->str, op_dest->str, op_src2->str);
+}
+
+make_helper(concat(shrd_i_, SUFFIX)) {
+	int len = concat(decode_si_rm2r_, SUFFIX) (eip + 1);  /* use decode_si_rm2r to read 1 byte immediate */
 	op_dest->val = REG(op_dest->reg);
-	op_src->str [0] ='%';
-	op_src->str [1] ='c';
-	op_src->str [2] ='l';
-	op_src->str [3] ='\0';
-	do_execute();
+	do_execute_l();
+	return len + 1;
+}
+
+static void do_execute_cl_l () {
+	DATA_TYPE in = op_src->val;
+	DATA_TYPE out = op_dest->val;
+	uint8_t count = MEM_R (reg_b (R_CL));
+	count &= 0x1f;
+	while(count != 0) {
+		out >>= 1;
+		out |= (in & 1) << ((DATA_BYTE << 3) - 1);
+		in >>= 1;
+		count --;
+	}
+
+	OPERAND_W(op_dest, out);
+
+	print_asm("shrd" str(SUFFIX) " $cl,%s,%s", op_dest->str, op_src->str);
+}
+make_helper(concat(shrd_cl_, SUFFIX)) {
+	int len = concat(decode_rm2r_, SUFFIX) (eip + 1);
+	do_execute_cl_l();
 	return len + 1;
 }
 #endif
