c14be6ab850e2616bc02f12848915db91fc21567
tracer-ics2015 <tracer@njuics.org>
2015-11-28 21:07:50 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:07:50 up 12:24,  2 users,  load average: 0.09, 0.12, 0.12 4638643aafa1e37512421907b72fe3948a66659

--- nemu/src/cpu/exec/con-trans/jae-template.h
+++ nemu/src/cpu/exec/con-trans/jae-template.h
@@ -6,7 +6,7 @@
 static void do_execute() {
 	DATA_TYPE_S displacement = op_src->val;
 	print_asm("jae %x",cpu.eip + 1 + DATA_BYTE + displacement);
-	if (cpu.ZF == 0 || cpu.CF == 1)cpu.eip +=displacement;
+	if (cpu.ZF == 0 || cpu.CF == 0)cpu.eip +=displacement;
 }
 make_instr_helper(i)
 
