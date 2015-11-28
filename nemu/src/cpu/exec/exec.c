3d4a6ddadcbc75c18e7d84b6d1e64eea20f33eca
tracer-ics2015 <tracer@njuics.org>
2015-11-28 12:25:27 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:25:27 up  7:29,  2 users,  load average: 0.18, 0.13, 0.13 5aacffcfced222888687443516ef23bc9dd5772

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -64,12 +64,12 @@ make_group(group2_cl_v,
 
 /* 0xf6 */
 make_group(group3_b,
-	inv, inv, not_rm_b, inv, 
+	test_i2rm_b, inv, not_rm_b, inv, 
 	mul_rm_b, imul_rm2a_b, inv, idiv_rm_b)
 
 /* 0xf7 */
 make_group(group3_v,
-	inv, inv, not_rm_v, inv, 
+	test_i2rm_v, inv, not_rm_v, inv, 
 	mul_rm_b, imul_rm2a_v, inv, idiv_rm_v)
 
 /* 0xfe */
