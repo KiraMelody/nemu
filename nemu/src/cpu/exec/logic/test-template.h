c669888faa860d12fbbf909dad42f0de7cb818f1
tracer-ics2015 <tracer@njuics.org>
2015-11-29 09:42:19 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  09:42:19 up 16:07,  2 users,  load average: 0.13, 0.10, 0.12 3b12b135113c617dacaf87f5f30a5e09cce31d91

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -4,9 +4,15 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_dest->val & op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
 	cpu.CF=0;
 	cpu.OF=0;
-	cpu.ZF=!result;
+	cpu.SF=result >> len;
+    	cpu.ZF=!result;
+    	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
 	print_asm_no_template2();
 }
 make_instr_helper(i2a)
