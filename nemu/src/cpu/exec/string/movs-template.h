6e05464d0c99ae165125ec21b495af2e092621a4
tracer-ics2015 <tracer@njuics.org>
2015-11-29 10:07:06 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:07:06 up 16:32,  2 users,  load average: 0.18, 0.16, 0.14 befbdd942bd0097b144aad6955566cc79bae9f2b

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute () {
 	MEM_W (REG (R_EDI),MEM_R (REG (R_ESI)));
-	printf ("%d %d\n",REG (R_EDI),MEM_R (REG (R_ESI)));
+	printf ("%x %x\n",REG (R_EDI),MEM_R (REG (R_ESI)));
 	REG (R_EDI) += DATA_BYTE;
 	REG (R_ESI) += DATA_BYTE;
 	print_asm("movs");
