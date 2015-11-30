db73f6c5908cb00d5e9fb497a244e5673bfdf800
tracer-ics2015 <tracer@njuics.org>
2015-12-01 01:06:52 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:06:52 up  5:31,  2 users,  load average: 0.33, 0.16, 0.15 f4570b3e32decd5022b1d491419bde805425e949

--- nemu/src/cpu/exec/con-trans/call-template.h
+++ nemu/src/cpu/exec/con-trans/call-template.h
@@ -7,9 +7,16 @@ static void do_execute() {
 	REG (R_ESP) -= DATA_BYTE;
 	MEM_W (REG (R_ESP) , cpu.eip + DATA_BYTE);
 	DATA_TYPE_S displacement = op_src->val;
-	printf ("%d\n",op_src->type);
-	print_asm("call %x",cpu.eip + 1 + DATA_BYTE + displacement);
-	cpu.eip +=displacement;
+	if (op_src->type == OP_TYPE_IMM)
+	{
+		print_asm("call %x",cpu.eip + 1 + DATA_BYTE + displacement);
+		cpu.eip +=displacement;
+	}
+	else
+	{
+		print_asm("call %x",displacement);
+		cpu.eip =displacement;
+	}
 }
 make_instr_helper(i)
 make_instr_helper(rm)
