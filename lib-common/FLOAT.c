f63658714a0872deea3fbbf38e63e9b714a832f9
tracer-ics2015 <tracer@njuics.org>
2015-12-02 16:04:37 +0800

    >  run 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:04:37 up 14:02,  2 users,  load average: 0.08, 0.05, 0.08 ea88e1d4106a9d45bac8c995b0d175b31b174812

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,8 +1,8 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	long long c = a * b;
-	return c >> 16;
+	long long c = (long long)a * (long long)b;
+	return (FLOAT)(c >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
