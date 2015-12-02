fb88f5a4503ef4d8341e01513c7a4c17e8aa5705
tracer-ics2015 <tracer@njuics.org>
2015-12-02 11:45:34 +0800

    >  run 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:45:34 up 10:45,  2 users,  load average: 0.37, 0.17, 0.15 7da2a57434cee70a8d3795b4c07ab58f18ad264c

---
+++ testcase/src/float_test.c
@@ -0,0 +1,18 @@
+#include "trap.h"
+#include "FLOAT.h"
+
+
+int main() { 
+	FLOAT a,b,c;
+	float d=1.2;
+	//float ans = 1.44;
+	a=f2F (d);
+	b=int2F (1);
+	c=int2F (-1);
+	nemu_assert(Fabs(c) == b);
+	nemu_assert(F_mul_int(a,1) == a);
+	//nemu_assert(Fabs (F_mul_F(a,b) - a ) < Fabs (f2F (1e-4)));
+	//nemu_assert(Fabs (F_mul_F(a,a) - f2F (ans) ) < Fabs (f2F (1e-4)));
+	HIT_GOOD_TRAP;
+	return 0;
+}
