6c23ab6c3511fab9ac2a2609fa703ced3b8391e5
tracer-ics2015 <tracer@njuics.org>
2015-12-02 16:11:13 +0800

    >  run 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:11:13 up 14:08,  2 users,  load average: 0.06, 0.07, 0.08 3d5e03b64c9e0c381b6b8878f252f6ba890893fa

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -19,21 +19,9 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 }
 
 int main() { 
-	FLOAT a,b,c;
-	float d=1.2;
-	float ans = 1.44;
-	a=f2F (d);
-	b=int2F (1);
-	c=int2F (-1);
-	nemu_assert(Fabs (Fabs(c)-b)<f2F (1e-4));
-	nemu_assert(F_mul_int(a,1) == a);
-	nemu_assert(Fabs (F_mul_F(a,b) - a ) < Fabs (f2F (1e-4)));
-	nemu_assert(Fabs (F_mul_F(a,a) - f2F (ans) ) < f2F (1e-4));
-	HIT_GOOD_TRAP;
 
-
-	//FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
-	//FLOAT ans = f2F(0.551222);
+	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
+	FLOAT ans = f2F(0.551222);
 
 	nemu_assert(Fabs(a - ans) < f2F(1e-4));
 	HIT_GOOD_TRAP;
