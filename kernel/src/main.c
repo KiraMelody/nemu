17bcb9db8c16c88a55cd86500c4cdeb5fcac49ce
tracer-ics2015 <tracer@njuics.org>
2016-01-11 20:54:15 +0800

    >  compile kernel 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:54:15 up  9:33,  2 users,  load average: 0.19, 0.18, 0.16 d52949bac175015fab8fe321a7dcb920759be53d

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -25,7 +25,7 @@ void init() {
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
 	init_page();
-
+	Log("fuck!");
 	/* After paging is enabled, transform %esp to virtual address. */
 	asm volatile("addl %0, %%esp" : : "i"(KOFFSET));
 #endif
