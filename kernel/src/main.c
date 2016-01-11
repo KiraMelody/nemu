bae2f6f36b64d6d03bfe0aaab40661cf03490edd
tracer-ics2015 <tracer@njuics.org>
2016-01-11 23:09:26 +0800

    >  compile kernel 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:09:26 up 11:48,  2 users,  load average: 0.10, 0.16, 0.12 2e204ce8f3eac3141f5be9f2d7d7d9d55e931b73

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -30,8 +30,7 @@ void init() {
 //#endif
 
 	/* Jump to init_cond() to continue initialization. */
-	panic("should not reach here");
-	Log("fuck!");
+	panic();
 	asm volatile("jmp *%0" : : "r"(init_cond));
 
 	panic("should not reach here");
