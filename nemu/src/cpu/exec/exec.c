910c649bd0999c24b7b10320ded5a2745cc97a55
tracer-ics2015 <tracer@njuics.org>
2016-01-11 22:27:43 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:27:43 up 11:07,  2 users,  load average: 0.15, 0.11, 0.09 1b303117f8f2aa8ac76a408476e9c357f012f001

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -19,18 +19,18 @@ static make_helper(_2byte_esc);
 	
 /* 0x80 */
 make_group(group1_b,
-	add_i2rm_b, inv, adc_i2rm_b, sbb_i2rm_b, 
-	and_i2rm_b, sub_i2rm_b, inv, cmp_i2rm_b)
+	add_i2rm_b, or_i2rm_b, adc_i2rm_b, sbb_i2rm_b, 
+	and_i2rm_b, sub_i2rm_b, xor_i2rm_b, cmp_i2rm_b)
 
 /* 0x81 */
 make_group(group1_v,
-	add_i2rm_v, inv, adc_i2rm_v, sbb_i2rm_v, 
-	and_i2rm_v, sub_i2rm_v, inv, cmp_i2rm_v)
+	add_i2rm_v, or_i2rm_v, adc_i2rm_v, sbb_i2rm_v, 
+	and_i2rm_v, sub_i2rm_v, xor_i2rm_v, cmp_i2rm_v)
 
 /* 0x83 */
 make_group(group1_sx_v,
-	add_si2rm_v, inv, adc_si2rm_v, sbb_si2rm_v, 
-	and_si2rm_v, sub_si2rm_v, inv, cmp_si2rm_v)
+	add_si2rm_v, or_si2rm_v, adc_si2rm_v, sbb_si2rm_v, 
+	and_si2rm_v, sub_si2rm_v, xor_si2rm_v, cmp_si2rm_v)
 
 /* 0xc0 */
 make_group(group2_i_b,
