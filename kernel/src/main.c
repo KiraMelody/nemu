b30b46e4770b551a44b8f63f29b96f773cb8f3a7
tracer-ics2015 <tracer@njuics.org>
2016-01-11 22:11:46 +0800

    >  compile kernel 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:11:46 up 10:51,  2 users,  load average: 0.26, 0.14, 0.10 edc3d3b932cff0151c506984819b2fde851cf391

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -20,14 +20,14 @@ void init_cond();
  * The assembly code in start.S will finally jump here.
  */
 void init() {
-#ifdef IA32_PAGE
+//#ifdef IA32_PAGE
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
 	init_page();
 	/* After paging is enabled, transform %esp to virtual address. */
 	asm volatile("addl %0, %%esp" : : "i"(KOFFSET));
-#endif
+//#endif
 
 	/* Jump to init_cond() to continue initialization. */
 	panic("should not reach here");
