c1a7dbd198ade1cf44f97b083c258a65bebf7d3e
tracer-ics2015 <tracer@njuics.org>
2016-01-11 22:41:53 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:41:53 up 11:21,  2 users,  load average: 0.11, 0.10, 0.07 c85b29005067b86a4c8293e5e0a55049d4125d8c

---
+++ nemu/src/cpu/exec/logic/std.c
@@ -0,0 +1,14 @@
+#include "cpu/exec/helper.h"
+
+#define DATA_BYTE 1
+
+
+#undef DATA_BYTE
+
+/* for instruction encoding overloading */
+
+make_helper(std)
+{
+	cpu.DF = 1;
+	return 1;
+}
---
+++ nemu/src/cpu/exec/logic/std.h
@@ -0,0 +1,6 @@
+#ifndef __std_H__
+#define __std_H__
+
+make_helper(std);
+
+#endif
