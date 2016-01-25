96887b475a64a1dd65bb471e9393b635548c915f
tracer-ics2015 <tracer@njuics.org>
2016-01-25 22:40:13 +0800

    >  compile kernel 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:40:13 up 2 days, 15:42,  2 users,  load average: 0.09, 0.12, 0.12 7b75778c31b569a02dc1ae6dda9d4567424db58

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -26,7 +26,7 @@ void init() {
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
-	//init_page();
+	init_page();
 	asm ("nop");
 	/* After paging is enabled, transform %esp to virtual address. */
 	//asm volatile("addl %0, %%esp" : : "i"(KOFFSET));
