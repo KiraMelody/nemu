d4e45d0a36b7488bbbf0522641eb652638d4671b
tracer-ics2015 <tracer@njuics.org>
2016-01-26 20:37:22 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:37:22 up 2 days, 23:15,  3 users,  load average: 0.38, 0.18, 0.15 cc26231de08572c288263207b45eca72c6bab281

--- Makefile
+++ Makefile
@@ -52,7 +52,7 @@ clean: clean-cpp
 
 ##### some convinient rules #####
 
-USERPROG := obj/testcase/wanshu
+USERPROG := obj/testcase/struct
 ENTRY := $(kernel_BIN)
 
 entry: $(ENTRY)
--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -3,14 +3,20 @@
 #define instr movzx
 
 static void do_movzx_b() {
-	DATA_TYPE_S result;
+	DATA_TYPE result;
 	result = op_src->val & 0xff;
+	if (op_dest->size == 1)result = (uint8_t)result;
+	else if (op_dest->size == 2)result = (uint16_t)result;
+	else result = (uint32_t)result;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
 static void do_movzx_w() {
-	DATA_TYPE_S result;
+	DATA_TYPE result;
 	result = op_src->val & 0xffff;
+	if (op_dest->size == 1)result = (uint8_t)result;
+	else if (op_dest->size == 2)result = (uint16_t)result;
+	else result = (uint32_t)result;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
