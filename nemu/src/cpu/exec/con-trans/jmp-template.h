bac0ae5e7a2e2476468d59cf499d010e586027e6
tracer-ics2015 <tracer@njuics.org>
2016-01-26 23:18:34 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:18:34 up 3 days,  1:40,  4 users,  load average: 0.13, 0.14, 0.14 8f1862e1fec903b65d932747e1d6ec2e27001a33

--- nemu/src/cpu/exec/con-trans/jmp-template.h
+++ nemu/src/cpu/exec/con-trans/jmp-template.h
@@ -7,11 +7,13 @@ static void do_execute() {
 	DATA_TYPE_S displacement = op_src->val;
 	if (op_src->type == OP_TYPE_REG || op_src->type == OP_TYPE_MEM)
 	{
+		printf ("dis: 0x%x\n",concat (decode_rm_,SUFFIX)(cpu.eip));
 		cpu.eip = displacement - concat (decode_rm_,SUFFIX)(cpu.eip) - 1;
 		print_asm_no_template1();
 	}
 	else
 	{
+		printf ("dis: 0x%x\n",displacement);
 		print_asm("jmp %x",cpu.eip + 1 + DATA_BYTE + displacement);
 		cpu.eip +=displacement;
 	}
