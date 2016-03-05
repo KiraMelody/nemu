fc509f7c13262885e1abc46caebaabf02c6fd413
tracer-ics2015 <tracer@njuics.org>
2016-03-05 10:32:19 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:32:19 up 11:53,  2 users,  load average: 0.10, 0.07, 0.05 e608d6cc5cfd19e9cdf723a9e8013a60ff830891

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -6,30 +6,30 @@ static void do_execute () {
 	if (ops_decoded.is_stack_size_16)
 	{
 		swaddr_write (reg_w(R_DI),2,swaddr_read (reg_w(R_SI),4));
-		/*if (cpu.DF == 0)
+		if (cpu.DF == 0)
+		{
+			reg_w (R_DI) += DATA_BYTE;
+			reg_w (R_SI) += DATA_BYTE;
+		}
+		else
 		{
 			reg_w (R_DI) -= DATA_BYTE;
 			reg_w (R_SI) -= DATA_BYTE;
 		}
-		else
-		{*/
-			reg_w (R_DI) += DATA_BYTE;
-			reg_w (R_SI) += DATA_BYTE;
-		//}
 	}
 	else
 	{
 		swaddr_write (reg_l(R_EDI),4,swaddr_read (reg_l(R_ESI),4));
-		/*if (cpu.DF == 0)
+		if (cpu.DF == 0)
+		{
+			reg_l (R_EDI) += DATA_BYTE;
+			reg_l (R_ESI) += DATA_BYTE;
+		}
+		else
 		{
 			reg_l (R_EDI) -= DATA_BYTE;
 			reg_l (R_ESI) -= DATA_BYTE;
 		}
-		else
-		{*/
-			reg_l (R_EDI) += DATA_BYTE;
-			reg_l (R_ESI) += DATA_BYTE;
-		//}
 
 	}
 	
