9d303384f974186ef47420d1749d13fa1a5611af
tracer-ics2015 <tracer@njuics.org>
2016-03-16 19:16:21 +0800

    >  compile kernel 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:16:21 up 2 days, 21:38,  2 users,  load average: 0.03, 0.15, 0.13 f5e7780f7a5f78425f712d12ae3e5b07b13d9b3d

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -2,7 +2,7 @@
 #define __COMMON_H__
 
 /* Uncomment these macros to enable corresponding functionality. */
-//#define IA32_SEG
+#define IA32_SEG
 //#define IA32_PAGE
 //#define IA32_INTR
 //#define HAS_DEVICE
