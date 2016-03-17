2ae2f462959fb0d1cce79c9236dca53fca5a3f7e
tracer-ics2015 <tracer@njuics.org>
2016-03-17 22:32:11 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:32:11 up 2 days, 22:46,  2 users,  load average: 0.18, 0.18, 0.20 588908c9ecdb2172eccc6f4211eebfb8d4d782d

--- nemu/src/cpu/exec/data-mov/movsr.c
+++ nemu/src/cpu/exec/data-mov/movsr.c
@@ -4,12 +4,17 @@
 
 make_helper(movsr) {
 	write_operand_l(op_src, cpu.CR0);
+	printf ("movsr\n");
 	print_asm("mov CR0,%s",op_src->str);
 	return 5;
 }
 
 make_helper(movsw) {
 	cpu.CR0 = op_src->val;
+	printf ("movsw\n");
+	if (cpu.PE == 1){
+
+	}
 	print_asm("mov %s,CR0",op_src->str);
 	return 5;
 }
