c2221758be5ffac7ade45fe38ab4e3dbc882b589
tracer-ics2015 <tracer@njuics.org>
2016-03-30 00:16:04 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:16:04 up 4 days, 20:30,  2 users,  load average: 0.17, 0.17, 0.16 9e15e1046cefa45ea9863d352a965d6829c70e21

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -226,22 +226,25 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, SELECTOR current_sreg) {
 	if (cpu.cr0.protect_enable == 0)return addr;
 	if (current_sreg.val == cpu.cs.selector) 
 	{
-		printf ("0x%x\n",cpu.cs.seg_limit);
+		printf ("cs 0x%x\n",cpu.cs.seg_limit);
 		Assert(addr+len < cpu.cs.seg_limit, "segment out limit, %x, %d, %x", addr, (int)len, cpu.cs.seg_limit);
 		return cpu.cs.seg_base+ addr;
 	}
 	else if (current_sreg.val == cpu.ds.selector) 
 	{
+		printf ("ds 0x%x\n",cpu.ds.seg_limit);
 		Assert(addr+len < cpu.ds.seg_limit, "segment out limit, %x, %d, %x", addr, (int)len, cpu.ds.seg_limit);
 		return cpu.ds.seg_base+ addr;
 	}
 	else if (current_sreg.val == cpu.es.selector) 
 	{
+		printf ("es 0x%x\n",cpu.es.seg_limit);
 		Assert(addr+len < cpu.es.seg_limit, "segment out limit, %x, %d, %x", addr, (int)len, cpu.es.seg_limit);
 		return cpu.es.seg_base+ addr;
 	}
 	else if (current_sreg.val == cpu.ss.selector) 
 	{
+		printf ("ss 0x%x\n",cpu.ss.seg_limit);
 		Assert(addr+len < cpu.ss.seg_limit, "segment out limit, %x, %d, %x", addr, (int)len, cpu.ss.seg_limit);
 		return cpu.ss.seg_base+ addr;
 	}
