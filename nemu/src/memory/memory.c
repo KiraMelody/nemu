74c48861c13ee7c73ac8ff092e06176a747f5e43
tracer-ics2015 <tracer@njuics.org>
2016-03-30 12:32:23 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:32:23 up 4 days, 21:02,  2 users,  load average: 0.07, 0.13, 0.19 a47ac46e8412ffb4b2ba8a4239d0b28ec75adb9c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -190,12 +190,12 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	uint32_t tmp = unalign_rw(temp + zero, 4) & (~0u >> ((4 - len) << 3)); 
 	uint32_t ground = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 	Assert (tmp == ground,"cache = 0x%x , dram = 0x%x , len = %d\n",tmp,ground,(int)len);
-	return tmp;
+	return ground;//cache fail
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	cache_write(addr, len, data);
-	//dram_write(addr, len, data);
+	dram_write(addr, len, data);//cache fail
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
