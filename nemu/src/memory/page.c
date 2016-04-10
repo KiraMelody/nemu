1da8004b991bb4380716a6f7869c1237be91cd02
tracer-ics2015 <tracer@njuics.org>
2016-04-10 19:49:06 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:49:06 up 5 days,  9:43,  2 users,  load average: 0.20, 0.15, 0.09 449e66d95721f1975d034c0cd9eef9061e90a856

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -11,7 +11,7 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	if (cpu.cr0.paging == 0)return addr;
 	//if ((hwaddr = readTLB(addr)) != -1)return hwaddr + OFFSET(addr);
 	dir.page_val = hwaddr_read((cpu.cr3.page_directory_base<<12)+(DIR(addr)<<2), 4);
-	Assert(dir.p, "dir do not exist at %x", cpu.eip);
+	Assert(dir.p, "pagevalue = %x eip = %x", dir.page_val,cpu.eip);
 	page.page_val = hwaddr_read((dir.addr<<12)+(PAGE(addr)<<2), 4);
 	Assert(page.p, "page do not exist at %x", cpu.eip);
 	hwaddr = (page.addr<<12)+OFFSET(addr);
