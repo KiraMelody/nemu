f9549847ffe49afa6585158d5b341941c49e826a
tracer-ics2015 <tracer@njuics.org>
2016-04-11 16:44:09 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:44:09 up 5 days, 13:55,  2 users,  load average: 0.02, 0.02, 0.05 6ed21f4ed28c5ffc075f4c149e27d603d1229f5

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -17,6 +17,6 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	hwaddr = (page.addr<<12)+OFFSET(addr);
 	//Assert(0,"value = %x",hwaddr);
 	//writeTLB(addr, hwaddr);
-	if (cpu.eip < 0x900000) Log ("addr = %x,dir = %x page = %x\n",addr,dir.page_val,page.page_val);
+	if (cpu.eip < 0x900000) Log ("0x%x: addr = %x,dir = %x page = %x result = %x\n",cpu.eip,addr,dir.page_val,page.page_val,hwaddr);
 	return hwaddr;
 }
