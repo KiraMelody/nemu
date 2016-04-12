cbdc3065227e0bfd721b33ca0e9f8967146b69c9
tracer-ics2015 <tracer@njuics.org>
2016-04-12 12:06:38 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:06:38 up 5 days, 21:12,  2 users,  load average: 0.28, 0.15, 0.15 311ace2b7e792387fddde777eb284ef552ec147d

--- nemu/src/cpu/exec/intr/int.c
+++ nemu/src/cpu/exec/intr/int.c
@@ -5,8 +5,8 @@ make_helper(intr) {
 	int NO = instr_fetch(eip + 1, 1);
 	printf ("no = %d\n",NO);
 	printf ("fuck1!\n");
-	if (cpu.cr0.protect_enable == 0)
-	{
+	//if (cpu.cr0.protect_enable == 0)
+	//{
 		printf ("fuck2!\n");
 		reg_l (R_ESP) -= 4;
 		swaddr_write (reg_l (R_ESP) , 4 , cpu.eflags);
@@ -17,12 +17,12 @@ make_helper(intr) {
 		reg_l (R_ESP) -= 4;
 		swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + 2);
 		raise_intr (NO);
-	}
-	else
-	{
-		printf ("fuck3!\n");
-		if (idt_des->privilege_level <=3)printf ("dpl = %d\n",idt_des->privilege_level);
-	}
+	// }
+	// else
+	// {
+	// 	printf ("fuck3!\n");
+	// 	if (idt_des->privilege_level <=3)printf ("dpl = %d\n",idt_des->privilege_level);
+	// }
 	printf ("fuck!\n");
 	print_asm("int %x",NO);
 	return 2;
