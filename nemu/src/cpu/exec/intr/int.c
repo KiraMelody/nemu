d18d51634c89b4a64639322a3e61fd1a23de403d
tracer-ics2015 <tracer@njuics.org>
2016-04-12 15:49:49 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:49:49 up 5 days, 22:20,  2 users,  load average: 0.11, 0.10, 0.13 57e501caee3602f0a40f8895edc4bfbe5e850b6b

--- nemu/src/cpu/exec/intr/int.c
+++ nemu/src/cpu/exec/intr/int.c
@@ -3,19 +3,10 @@
 void raise_intr(uint8_t);
 make_helper(intr) {
 	int NO = instr_fetch(eip + 1, 1);
-	printf ("no = %d\n",NO);
-	printf ("fuck1!\n");
 	//if (cpu.cr0.protect_enable == 0)
 	//{
-		printf ("fuck2!\n");
-		reg_l (R_ESP) -= 4;
-		swaddr_write (reg_l (R_ESP) , 4 , cpu.eflags);
 		cpu.IF = 0;
 		cpu.TF = 0;
-		reg_l (R_ESP) -= 4;
-		swaddr_write (reg_l (R_ESP) , 4 , cpu.cs.selector);
-		reg_l (R_ESP) -= 4;
-		swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + 2);
 		raise_intr (NO);
 	// }
 	// else
