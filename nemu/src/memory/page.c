22ce12d18e124ea3216845c0db31e33421e2d08d
tracer-ics2015 <tracer@njuics.org>
2016-04-12 17:04:33 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  17:04:33 up 5 days, 22:57,  2 users,  load average: 0.16, 0.12, 0.14 4f0e5d244977b9c66ac95d1aac4cf469a82fcb4e

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -4,17 +4,20 @@
 #define PAGE(addr) (((addr)>>12) & 0x3ff)
 #define OFFSET(addr) ((addr) & 0xfff)
 
+hwaddr_t TLB_read(uint32_t);
+void TLB_write(uint32_t ,uint32_t);
+
 hwaddr_t page_translate(lnaddr_t addr) {
 	PAGE_descriptor dir;
 	PAGE_descriptor page;
 	hwaddr_t hwaddr;
 	if (!cpu.cr0.paging || !cpu.cr0.protect_enable)return addr;
-	//if ((hwaddr = readTLB(addr)) != -1)return hwaddr + OFFSET(addr);
+	if ((hwaddr = TLB_read(addr)) != -1)return hwaddr + OFFSET(addr);
 	dir.page_val = hwaddr_read((cpu.cr3.page_directory_base<<12)+(DIR(addr)<<2), 4);
 	Assert(dir.p, "pagevalue = %x eip = %x", dir.page_val,cpu.eip);
 	page.page_val = hwaddr_read((dir.addr<<12)+(PAGE(addr)<<2), 4);
 	Assert(page.p, "page do not exist at %x", cpu.eip);
 	hwaddr = (page.addr<<12)+OFFSET(addr);
-	//writeTLB(addr, hwaddr);
+	TLB_write(addr, hwaddr);
 	return hwaddr;
 }
