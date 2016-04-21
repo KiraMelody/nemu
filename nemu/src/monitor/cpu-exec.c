fe8244dd83769e39df2605829bbbe0a51a91eae7
tracer-ics2015 <tracer@njuics.org>
2016-04-22 01:41:01 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:41:01 up 36 min,  2 users,  load average: 0.08, 0.10, 0.21 3cbda22ac26b9ae7fa7c1a1f557e9acfd9a7459

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -41,7 +41,7 @@ void raise_intr(uint8_t NO) {
 	Log ("cs.selector = %x\n",cpu.cs.selector);
 	Log ("idtr.base = %x\n",cpu.idtr.base_addr);
 	cpu.cs.selector = idt_des -> segment;
-	Assert(((cpu.cs.selector>>3)<<3) <= cpu.gdtr.seg_limit, "segment out limit %d, %d", ((cpu.cs.selector>>3)<<3), cpu.gdtr.seg_limit);
+	//Assert(((cpu.cs.selector>>3)<<3) <= cpu.gdtr.seg_limit, "segment out limit %d, %d", ((cpu.cs.selector>>3)<<3), cpu.gdtr.seg_limit);
 	seg_des->first_part = instr_fetch(cpu.gdtr.base_addr + ((cpu.cs.selector>>3)<<3), 4);
 	seg_des->second_part = instr_fetch(cpu.gdtr.base_addr + ((cpu.cs.selector>>3)<<3)+4, 4);
 	Assert(seg_des->p == 1, "segment error");
