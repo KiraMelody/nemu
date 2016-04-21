9bbd2927d52e2bab419dbbd3e10f86bdcd2297fd
tracer-ics2015 <tracer@njuics.org>
2016-04-22 02:15:55 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  02:15:55 up  1:11,  2 users,  load average: 0.26, 0.27, 0.19 241cb92ccc67193a703ec602ee263ad3a45b197

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -37,7 +37,7 @@ void raise_intr(uint8_t NO) {
 	idt_des->second_part = lnaddr_read(pidt, 4);
 	Log ("%x %x\n",idt_des->first_part ,idt_des->second_part);
 	uint64_t idt_des2 = ((uint64_t) lnaddr_read(pidt + 4, 4) << 32) | lnaddr_read(pidt, 4); 
-	Log ("%lx %lx\n",(uint64_t)idt_des,(uint64_t)idt_des2);
+	Log ("%x %lx\n",idt_des -> segment,((uint64_t)idt_des2 >> 16) & 0xFFFF);
     	//uint64_t idt_des = ((uint64_t) lnaddr_read(pidt + 4, 4) << 32) | lnaddr_read(pidt, 4); 
     	//Assert((idt_des >> 47) & 1, "IDT descripter does not present, Interrupt # = %#x", NO);
     	//uint8_t gate_type = (idt_des >> 40) & 0x7;
@@ -46,11 +46,11 @@ void raise_intr(uint8_t NO) {
 	push (cpu.eip); 
     // long jump
     	cpu.cs.selector = idt_des -> segment;
-    	Log ("%ld %ld\n",((uint64_t)idt_des >> 16) & 0xFFFF,(uint64_t)cpu.cs.selector);
+    	Log ("%ld %ld\n",((uint64_t)idt_des2 >> 16) & 0xFFFF,(uint64_t)cpu.cs.selector);
     	current_sreg = R_CS;
     	sreg_load();
     	//cpu.eip = cpu.cs.seg_base + idt_des -> offset_15_0 + (idt_des -> offset_31_16 << 16);
-    	cpu.eip = ((uint64_t)idt_des & 0xFFFF) | (((uint64_t)idt_des >> 32LL) & 0xFFFF0000);
+    	cpu.eip = ((uint64_t)idt_des2 & 0xFFFF) | (((uint64_t)idt_des2 >> 32LL) & 0xFFFF0000);
     /* Jump back to cpu_exec() */
     	longjmp(jbuf, 1);
 }
