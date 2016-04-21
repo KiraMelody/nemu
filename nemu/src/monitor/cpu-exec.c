b329808f0477367cb43e5041447cfd0e1d0fd1ac
tracer-ics2015 <tracer@njuics.org>
2016-04-22 02:18:33 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  02:18:33 up  1:14,  2 users,  load average: 0.22, 0.26, 0.21 823ee99e27bab274a6c2fd4bb6536317512fd72

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -33,8 +33,8 @@ void raise_intr(uint8_t NO) {
     	GATE_descriptor gate;
 	idt_des = &gate;
    	lnaddr_t pidt = cpu.idtr.base_addr + NO * 8;
-   	idt_des->first_part = lnaddr_read(pidt + 4, 4);
-	idt_des->second_part = lnaddr_read(pidt, 4);
+   	idt_des->first_part = lnaddr_read(pidt, 4);
+	idt_des->second_part = lnaddr_read(pidt + 4, 4);
 	Log ("%x %x\n",idt_des->first_part ,idt_des->second_part);
 
 	uint64_t idt_des2 = ((uint64_t) lnaddr_read(pidt + 4, 4) << 32) | lnaddr_read(pidt, 4); 
