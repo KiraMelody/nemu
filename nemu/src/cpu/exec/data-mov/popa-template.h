d7db081e81574f5dcde4f5df6d42ea1a428223de
tracer-ics2015 <tracer@njuics.org>
2016-04-22 15:49:40 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:49:40 up  4:51,  2 users,  load average: 0.15, 0.14, 0.14 449442157aca2107e2df5acdae79ca3a29511648

--- nemu/src/cpu/exec/data-mov/popa-template.h
+++ nemu/src/cpu/exec/data-mov/popa-template.h
@@ -4,7 +4,7 @@
 #if DATA_BYTE == 2
 static void pop(int x)
 {
-	//Log ("pop %s as %x",regs[x],reg_l (R_ESP));
+	Log ("pop %s as %x",regsl[x],reg_l (R_ESP));
 	current_sreg = R_SS;
 	reg_l (x) = swaddr_read(reg_l (R_ESP) , 4);
 	swaddr_write (reg_l (R_ESP) , 4 , 0);
