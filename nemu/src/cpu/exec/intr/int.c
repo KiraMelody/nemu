f385666059e17d4b82a5fba5c4a9270c2428cb0f
tracer-ics2015 <tracer@njuics.org>
2016-04-22 09:14:39 +0800

    >  compile NEMU 14307130372 kiralla Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  09:14:39 up  2:13,  2 users,  load average: 0.17, 0.13, 0.19 2f0577d04d189a7b4001dd55dcfcf8adaf2642d

--- nemu/src/cpu/exec/intr/int.c
+++ nemu/src/cpu/exec/intr/int.c
@@ -3,6 +3,9 @@
 void raise_intr(uint8_t);
 make_helper(intr) {
 	int NO = instr_fetch(eip + 1, 1);
+	printf ("eip = %x\n",cpu.eip);
+	cpu.eip += 2;
+	printf ("eip = %x\n",cpu.eip);
 	raise_intr (NO);
 	print_asm("int %x",NO);
 	return 2;
